0c92050df1884de5c51185c07200a49d9bdee1e7
malixds <malixds@mail.ru>
2025-05-13 19:40:15 +0300

    db v2

--- up.sh
+++ up.sh
@@ -4,3 +4,14 @@ docker-compose run --rm npm install
 docker-compose run --rm npm run build
 docker-compose run --rm artisan migrate:fresh --seed
 docker-compose run --rm artisan optimize
+
+
+#docker compose exec php php artisan config:clear
+#docker compose exec php php artisan cache:clear
+#docker compose exec php php artisan optimize:clear
+
+#docker exec -it postgres psql -U postgres -d postgres
+
+#docker compose down -v
+
+
