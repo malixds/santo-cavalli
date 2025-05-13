d4159a80eeb6cbeea455b41844852429ebfb747e
malixds <malixds@mail.ru>
2025-05-13 19:53:58 +0300

    миграцииg

---
+++ src/database/migrations/2025_05_13_164818_create_designs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('designs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->text('description')->nullable();
+            $table->text('public_url');
+            $table->foreignId('category_id')->references('id')->on('categories')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('designs');
+    }
+};
