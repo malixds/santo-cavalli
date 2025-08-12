0a8f849dfb6c244a00ad7c41eaa7bda975f7e1da
malixds <malixds@mail.ru>
2025-08-12 17:14:07 +0300

    basic api methods for designs func

--- designs/routes/api.php
+++ designs/routes/api.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/designs')->group(function () {
     Route::post('/send', [DesignController::class, 'sendDesignByUser'])->name('design.user-send');
-    Route::post('/get', [DesignController::class, 'getAllUsersDesigns'])->name('design.user-get-all');
+    Route::get('/get/{design?}', [DesignController::class, 'getAllUsersDesigns'])->name('design.user-get-all');
+    Route::patch('/edit/{design}', [DesignController::class, 'editDesign'])->name('design.user-edit');
+    Route::delete('/delete/design', [DesignController::class, 'deleteDesign'])->name('design.user-delete');
     Route::prefix('/admins')->group(function () {
         Route::get('/show/{design?}', [DesignController::class, 'getDesigns'])->name('design.admin-get');
-        Route::get('/handle/{design}', [DesignController::class, 'getDesigns'])->name('design.admin-get');
+        Route::patch('/handle/{design}', [DesignController::class, 'handleDesign'])->name('design.admin-handle');
     });
 });
